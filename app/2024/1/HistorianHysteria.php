2ed8d893f75b3af5c6849f9f574408d85a54f2d1
Jakub Párys <jakub.parys@seznam.cz>
2024-12-01 18:02:58 +0100

    AoC - 2024 - Day 1 Part 2

--- app/2024/1/HistorianHysteria.php
+++ app/2024/1/HistorianHysteria.php
@@ -42,7 +42,30 @@
          */
         function getPartTwoResult(): int
         {
-            return 0;
+            $inputText = file( INPUTS_PATH . '/2024/1_input.txt' );
+            $sum = 0;
+
+            $leftStack = [];
+            $rightStack = [];
+
+            foreach ( $inputText as $line )
+            {
+                $nums = explode( '   ', trim( $line ) );
+
+                $leftStack[] = (int)$nums[ 0 ];
+                $rightStack[] = (int)$nums[ 1 ];
+            }
+
+            foreach ( $leftStack as $leftNum )
+            {
+                $multiplier = count( array_filter( $rightStack, function ( $rightNum ) use ( $leftNum ) {
+                    return $rightNum === $leftNum;
+                } ) );
+
+                $sum += $leftNum * $multiplier;
+            }
+
+            return $sum;
         }
 
 
