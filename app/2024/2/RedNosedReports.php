1ddecfa678e99debba507bebed346f31a3ecf2a2
Jakub Párys <jakub.parys@seznam.cz>
2024-12-02 22:29:01 +0100

    AoC - 2024 - Day 2 Part 2

--- app/2024/2/RedNosedReports.php
+++ app/2024/2/RedNosedReports.php
@@ -33,7 +33,7 @@
                 foreach ( $nums as $num )
                 {
                     $num = (int)$num;
-                    
+
                     if ( $lastNumber && ( abs( $lastNumber - $num ) > 3 || abs( $lastNumber - $num ) === 0 ) )
                     {
                         $isSafe = false;
@@ -56,7 +56,70 @@
          */
         function getPartTwoResult(): int
         {
-            return 0;
+            $inputText = file( INPUTS_PATH . '/2024/2_input.txt' );
+            $sum = 0;
+
+            foreach ( $inputText as $line )
+            {
+                $nums = explode( ' ', trim( $line ) );
+                $isSafe = $this->checkNumbers( $nums );
+
+                if ( !$isSafe )
+                {
+                    foreach ( $nums as $key => $num )
+                    {
+                        $oneNumRemoved = $nums;
+                        unset( $oneNumRemoved[ $key ] );
+
+                        $isSafeWithOneMistake = $this->checkNumbers( array_values( $oneNumRemoved ) );
+
+                        if ( $isSafeWithOneMistake )
+                        {
+                            $isSafe = true;
+                            break;
+                        }
+                    }
+                }
+
+                $sum += $isSafe === true ? 1 : 0;
+            }
+
+            return $sum;
+        }
+
+
+
+        /**
+         * @param array $nums
+         * @return bool
+         */
+        public function checkNumbers( array $nums ): bool
+        {
+            $lastNumber = null;
+            $sortedAsc = $nums;
+            $sortedDesc = $nums;
+
+            sort( $sortedAsc, SORT_NUMERIC );
+            rsort( $sortedDesc, SORT_NUMERIC );
+
+            if ( $nums !== $sortedAsc && $nums !== $sortedDesc )
+            {
+                return false;
+            }
+
+            foreach ( $nums as $num )
+            {
+                $num = (int)$num;
+
+                if ( $lastNumber && ( abs( $lastNumber - $num ) > 3 || abs( $lastNumber - $num ) === 0 ) )
+                {
+                    return false;
+                }
+
+                $lastNumber = $num;
+            }
+
+            return true;
         }
 
 
