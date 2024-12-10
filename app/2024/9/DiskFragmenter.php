dcf7f6e1c11ac2748c565a5431a20fc044a69caf
Jakub Párys <jakub.parys@seznam.cz>
2024-12-11 00:35:34 +0100

    AoC - 2024 - Day 9 Part 2

--- app/2024/9/DiskFragmenter.php
+++ app/2024/9/DiskFragmenter.php
@@ -60,7 +60,76 @@
          */
         function getPartTwoResult(): int
         {
-            return 0;
+            $sum = 0;
+            $input = file_get_contents( INPUTS_PATH . '/2024/9_input.txt', FILE_IGNORE_NEW_LINES );
+
+            $input = $this->parseInputV2( $input );
+            $diskMap = $input[ 'disk_map' ];
+            $emptySlots = $input[ 'empty_slots' ];
+            $res = [];
+
+            for ( $i = count( $diskMap ) - 1; $i >= 0; $i-- )
+            {
+                $chunkLen = count( $diskMap[ $i ] );
+                $freeSlot = $this->getFirstAvailableSlot( $emptySlots, $chunkLen, $i );
+
+                $slot = array_fill( 0, $chunkLen, $i );
+
+                if ( $freeSlot !== null )
+                {
+                    $res[ $freeSlot ] = array_merge( isset( $res[ $freeSlot ] ) ? array_values( $res[ $freeSlot ] ) : [], $slot );
+                    $res[ $i ] = array_merge( array_fill( 0, $chunkLen, 0 ), isset( $res[ $i ] ) ? array_values( $res[ $i ] ) : [] );
+                }
+                else
+                {
+                    $res[ $i ] = array_merge( $slot, isset( $res[ $i ] ) ? array_values( $res[ $i ] ) : [] );
+                }
+            }
+
+            foreach ( $emptySlots as $key => $e )
+            {
+                $res[ $key ] = array_merge( isset( $res[ $key ] ) ? array_values( $res[ $key ] ) : [], array_fill( 0, $e, 0 ) );
+            }
+
+            ksort( $res );
+
+            $p = 0;
+            foreach ( $res as $slot )
+            {
+                foreach ( $slot as $id )
+                {
+                    $sum += $p * $id;
+                    $p++;
+                }
+            }
+
+            return $sum;
+        }
+
+
+
+        /**
+         * @param array $freeSlots
+         * @param int $chunkLen
+         * @param int $id
+         * @return int|null
+         */
+        private function getFirstAvailableSlot( array &$freeSlots, int $chunkLen, int $id ): ?int
+        {
+            $freeSlotsTmp = array_filter( $freeSlots, function ( $v, $k ) use ( $id ) {
+                return $k < $id;
+            }, ARRAY_FILTER_USE_BOTH );
+
+            foreach ( $freeSlotsTmp as $key => $freeSlotSpace )
+            {
+                if ( $freeSlotSpace >= $chunkLen )
+                {
+                    $freeSlots[ $key ] = $freeSlotSpace - $chunkLen;
+                    return $key;
+                }
+            }
+
+            return null;
         }
 
 
@@ -86,6 +155,33 @@
             }
 
             return [ 'disk_map' => $diskMap, 'max' => $max ];
+        }
+
+
+
+        /**
+         * @param string $input
+         * @return array
+         */
+        private function parseInputV2( string $input ): array
+        {
+            $diskMap = [];
+            $emptySlots = [];
+            $max = 0;
+
+            $input = str_split( $input, 2 );
+
+            foreach ( $input as $id => $block )
+            {
+                $diskMap[] = array_fill( 0, $block[ 0 ], $id );
+
+                if ( isset( $block[ 1 ] ) && $block[ 1 ] != 0 )
+                {
+                    $emptySlots[ $id ] = (int)$block[ 1 ];
+                }
+            }
+
+            return [ 'disk_map' => $diskMap, 'max' => $max, 'empty_slots' => $emptySlots ];
         }
 
 
