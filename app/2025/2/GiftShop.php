91f145282a432b6b7de89d0b7a360b3855c5e028
parysoid <jakub.parys@seznam.cz>
2025-12-05 20:23:39 +0100

    AoC - 2025 - Day 2 Part 2

--- app/2025/2/GiftShop.php
+++ app/2025/2/GiftShop.php
@@ -54,8 +54,48 @@
          */
         function getPartTwoResult(): int
         {
-            $inputText = trim( file_get_contents( INPUTS_PATH . '/2025/2_input.txt' ) );
+            $idRanges = explode( ',', file_get_contents( INPUTS_PATH . '/2025/2_input.txt' ) );
             $sum = 0;
+
+            foreach ( $idRanges as $idRange )
+            {
+                $idRange = explode( '-', $idRange );
+
+                $min = $idRange[ 0 ];
+                $max = $idRange[ 1 ];
+
+                for ( $i = $min; $i <= $max; $i++ )
+                {
+                    $str = (string)$i;
+                    $len = strlen( $str );
+
+                    if ( $len === 1 )
+                    {
+                        continue;
+                    }
+
+                    $num = str_split( (string)$i );
+
+                    if ( count( array_unique( $num ) ) === 1 )
+                    {
+                        $sum += $i;
+                        continue;
+                    }
+
+                    $chunks = (int)floor( count( $num ) / 2 );
+
+                    for ( $k = 2; $k <= $chunks; $k++ )
+                    {
+                        $numMirroring = array_chunk( $num, $k );
+
+                        if ( count( array_unique( $numMirroring, SORT_REGULAR ) ) === 1 )
+                        {
+                            $sum += $i;
+                            break;
+                        }
+                    }
+                }
+            }
 
             return $sum;
         }
