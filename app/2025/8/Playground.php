bb6db4fc00fd085db506ab1fa374dd5f120ce2aa
parysoid <jakub.parys@seznam.cz>
2025-12-16 16:56:15 +0100

    feat(2025): day 8 part 2

--- app/2025/8/Playground.php
+++ app/2025/8/Playground.php
@@ -52,11 +52,8 @@
                 $c++;
             }
 
-            return $connections->getResultTopProduct(  );
+            return $connections->getResultTopProduct();
         }
-
-
-
 
 
 
@@ -65,8 +62,43 @@
          */
         function getPartTwoResult(): int
         {
-            $lines = $this->readLines( 2025, 8 );
             $res = 0;
+            $lines = $this->readLines( 2025, 8 );
+            $distances = new Distances();
+            $boxesEntity = \Playground\Boxes::hydrateBoxes( $lines );
+            $boxes = array_values( $boxesEntity->getBoxes() );
+            $n = $boxesEntity->getBoxesCount();
+
+            for ( $i = 0; $i < $n; $i++ )
+            {
+                for ( $j = $i + 1; $j < $n; $j++ )
+                {
+                    $boxA = $boxes[ $i ];
+                    $boxB = $boxes[ $j ];
+
+                    $dist2 = ( $boxB->getX() - $boxA->getX() ) ** 2
+                        + ( $boxB->getY() - $boxA->getY() ) ** 2
+                        + ( $boxB->getZ() - $boxA->getZ() ) ** 2;
+
+                    $distances->attachDistance( new Distance( $boxA, $boxB, $dist2 ) );
+                }
+            }
+
+            $distances->sortDistances();
+
+            $connections = new Connections( $boxesEntity );
+            $connections->initBoxes( $boxesEntity->getBoxes() );
+
+            foreach ( $distances->getDistances() as $distance )
+            {
+                $connections->handleDistance( $distance );
+
+                if ( max( $connections->getSizes() ) === $n )
+                {
+                    $res = $distance->getBoxA()->getX() * $distance->getBoxB()->getX();
+                    break;
+                }
+            }
 
             return $res;
         }
